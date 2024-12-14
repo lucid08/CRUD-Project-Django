66a2ae2aa5f30bd29b3b6cbdfa443cc892ff92e8
Sumit Choudhary <sumit.rc619@gmail.com>
2024-12-14 23:02:34 +0530

    N

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ function ProtectedRoute({ children }) {
   const [isAuthorized, setIsAuthorized] = useState(null);
 
   useEffect(() => {
-    auth().catch(() => setIsAuthorized(false));
+    auth().catch(() => setIsAuthorized(fals));
   }, [])
 
   const refreshToken = async () => {
